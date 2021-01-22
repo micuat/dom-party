21ef5b05269cfc6add1fd58e1f35e44618429159
Glitch (hello-webpage) <none>
2021-01-22 14:39:52 +0000

    ⛳️🍁 Checkpoint ./script.js:9838890/4366 ./index.html:9838890/297

--- script.js
+++ script.js
@@ -7,27 +7,26 @@ let active = false;
 // Make the volume quieter
 Tone.Master.volume.value = volume;
 
-const codes = document.querySelectorAll(".code")
-for(const c of codes) {
+const codes = document.querySelectorAll(".code");
+for (const c of codes) {
   c.onclick = () => {
-  eval(c.innerText);
-  console.log("hi");
+    eval(c.innerText);
+    console.log("hi");
 
-  if (!active) {
-    active = true;
-  } else {
-    // active = false;
-  }
-  synth.triggerAttackRelease(freq, dur);
-};
+    if (!active) {
+      active = true;
+    } else {
+      // active = false;
+    }
+    synth.triggerAttackRelease(freq, dur);
+  };
 }
 class Synthesizer {
   constructor(outlet, source) {
     this.outlet = outlet;
-    if(source == undefined) {
+    if (source == undefined) {
       this.source = outlet;
-    }
-    else {
+    } else {
       this.source = source;
     }
   }
@@ -65,34 +64,50 @@ class Sine extends Synthesizer {
   }
 }
 
-const sine = (freq) => {
+const sine = freq => {
   return new Sine(freq);
-}
+};
 
 class AM extends Synthesizer {
-  constructor(f) {
-    const s = new Tone.AMSynth();
+  constructor(f, fm = 2) {
+    const s = new Tone.AMSynth({
+      modulation: {
+        type: Tone.square
+      }
+    });
     super(s);
     freq = f;
     dur = "8n";
-    s.harmonicity.value = 4/440
+    s.harmonicity.value = fm / f;
   }
 }
 
-const am = (freq) => {
-  return new AM(freq);
-}
+const am = (f, fm) => {
+  return new AM(f, fm);
+};
 
 class FM extends Synthesizer {
-  constructor(f) {
+  constructor(f, fm = 2) {
     const s = new Tone.FMSynth({});
     super(s);
     freq = f;
     dur = "8n";
+    s.harmonicity.value = fm / f;
   }
 }
 
-const fm = (freq) => {
-  return new FM(freq);
+const fm = (f, fm) => {
+  return new FM(f, fm);
+};
+
+class Noise extends Synthesizer {
+  constructor() {
+    const s = new Tone.NoiseSynth({});
+    super(s);
+    dur = "8n";
+  }
 }
 
+const noise = () => {
+  return new Noise();
+};
