cb2957ee37d8404f8b39885ea15b9acc63d1da25
Glitch (hello-webpage) <none>
2021-03-21 14:10:23 +0000

    🖼🏑 Checkpoint ./sound/script.js:9838890/11939

--- sound/script.js
+++ sound/script.js
@@ -1,16 +1,16 @@
-const windowId = Math.floor(Math.random()*65536*65536).toString(16);
+const windowId = Math.floor(Math.random() * 65536 * 65536).toString(16);
 
 // Create WebSocket connection.
-const socket = new WebSocket('ws://localhost:8080');
+const socket = new WebSocket("ws://localhost:8080");
 
 // Connection opened
-socket.addEventListener('open', function (event) {
-    socket.send({type: "browserconnection", windowId});
+socket.addEventListener("open", function(event) {
+  socket.send({ type: "browserconnection", windowId });
 });
 
 // Listen for messages
-socket.addEventListener('message', function (event) {
-    console.log('Message from server ', event.data);
+socket.addEventListener("message", function(event) {
+  console.log("Message from server ", event.data);
 });
 
 // hydra
@@ -31,9 +31,54 @@ cm.refresh();
 cm.setValue(`//sine(440).out()
 `);
 
-const startTime = new Date;
+const startTime = new Date();
 
-const vid = document.querySelector("video")
+const vid = document.querySelector("video");
+vid.addEventListener(
+  "play",
+  function() {
+    let frameCount = 0;
+    let logFps = 10;
+    let sendInterval = 10; // sec
+    let sendEvery = logFps * sendInterval;
+    let frames = [];
+    let startedAt = vid.currentTime;
+    let isChanged = false;
+    let lastXY = { x: -1, y: -1 };
+
+    setInterval(() => {
+      let x = mouseX;
+      let y = mouseY;
+      frames.push({ x, y });
+      if (lastXY.x != x || lastXY.y != y) {
+        isChanged = true;
+        lastXY = { x, y };
+      }
+      frameCount++;
+      if (frameCount >= sendEvery) {
+        let fps = logFps;
+        if (isChanged == false) {
+          frames = [{ x, y }];
+          fps = 1 / sendInterval;
+        }
+
+        const command = {
+          type: "browsermouse",
+          windowId,
+          startedAt,
+          fps,
+          values: frames
+        };
+        socket.send(JSON.stringify(command));
+        startedAt = vid.currentTime;
+        frames = [];
+        frameCount = 0;
+        isChanged = false;
+      }
+    }, 1000 / logFps);
+  },
+  true
+);
 
 // https://github.com/ojack/hydra/blob/3dcbf85c22b9f30c45b29ac63066e4bbb00cf225/hydra-server/app/src/editor.js
 const flashCode = function(start, end) {
@@ -81,30 +126,48 @@ window.onkeydown = e => {
   if (cm.hasFocus()) {
     const t = vid.currentTime;
     // const t = new Date - startTime;
-    const command = {type: "synth", windowId, cursor: cm.getCursor(), code: cm.getValue(), t};
+    const command = {
+      type: "synth",
+      windowId,
+      cursor: cm.getCursor(),
+      code: cm.getValue(),
+      t
+    };
     if (e.keyCode === 13) {
       e.preventDefault();
       if (e.ctrlKey === true && e.shiftKey === true) {
         // ctrl - shift - enter: evalAll
         const code = cm.getValue();
         flashCode();
-        try{eval(code)}catch(e){console.log(e)};
+        try {
+          eval(code);
+        } catch (e) {
+          console.log(e);
+        }
         command.eval = code;
         command.exec = "ctrl-shift-enter";
         // hydra.eval(code);
       } else if (e.ctrlKey === true && e.shiftKey === false) {
         // ctrl - enter: evalLine
         const code = getLine();
-        try{eval(code)}catch(e){console.log(e)};
+        try {
+          eval(code);
+        } catch (e) {
+          console.log(e);
+        }
         command.eval = code;
         command.exec = "ctrl-enter";
       } else if (e.altKey === true) {
         // alt - enter: evalBlock
         const code = getCurrentBlock();
-        try{eval(code)}catch(e){console.log(e)};
+        try {
+          eval(code);
+        } catch (e) {
+          console.log(e);
+        }
         command.eval = code;
         command.exec = "alt-enter";
-      }else if (e.shiftKey === true) {
+      } else if (e.shiftKey === true) {
         // shift - enter: hush
         hushSound();
         command.eval = "hushSound()";
@@ -117,6 +180,13 @@ window.onkeydown = e => {
 
 container.onclick = e => {
   const t = vid.currentTime;
-  const command = {type: "synth", windowId, clicked: true, cursor: cm.getCursor(), code: cm.getValue(), t};
+  const command = {
+    type: "synth",
+    windowId,
+    clicked: true,
+    cursor: cm.getCursor(),
+    code: cm.getValue(),
+    t
+  };
   socket.send(JSON.stringify(command));
-}
+};
